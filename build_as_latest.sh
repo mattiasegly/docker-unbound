6c217586f1a8c8fdf0468afbafd4e500f9234aba
Mattias Egly <48134992+mattiasegly@users.noreply.github.com>
2022-01-13 15:43:27 +0100

    Build script updates

---
+++ build_as_latest.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+DOCKER_REPO=mattiasegly/rpi-unbound
+SOURCE_BRANCH=buster
+
+#Pull base images
+for ARCH in amd64 386 arm
+do
+docker pull mattiasegly/base-image:$SOURCE_BRANCH-$ARCH
+done
+
+#Start fresh
+for ARCH in amd64 386 arm
+do
+docker image rm $DOCKER_REPO:$SOURCE_BRANCH-$ARCH
+done
+
+#Build and push
+for ARCH in amd64 386 arm
+do
+docker build -f Dockerfile.$ARCH -t $DOCKER_REPO:$SOURCE_BRANCH-$ARCH .
+docker push $DOCKER_REPO:$SOURCE_BRANCH-$ARCH
+done
+
+#Release specific tag
+docker manifest create \
+	$DOCKER_REPO:$SOURCE_BRANCH \
+	$DOCKER_REPO:$SOURCE_BRANCH-amd64 \
+	$DOCKER_REPO:$SOURCE_BRANCH-386 \
+	$DOCKER_REPO:$SOURCE_BRANCH-arm
+
+for ARCH in amd64 386 arm
+do
+docker manifest annotate \
+	$DOCKER_REPO:$SOURCE_BRANCH \
+	$DOCKER_REPO:$SOURCE_BRANCH-$ARCH --os linux --arch $ARCH
+done
+
+docker manifest push --purge \
+	$DOCKER_REPO:$SOURCE_BRANCH
+
+#Default "latest" tag
+docker manifest create \
+	$DOCKER_REPO \
+	$DOCKER_REPO:$SOURCE_BRANCH-amd64 \
+	$DOCKER_REPO:$SOURCE_BRANCH-386 \
+	$DOCKER_REPO:$SOURCE_BRANCH-arm
+
+for ARCH in amd64 386 arm
+do
+docker manifest annotate \
+	$DOCKER_REPO \
+	$DOCKER_REPO:$SOURCE_BRANCH-$ARCH --os linux --arch $ARCH
+done
+
+docker manifest push --purge \
+	$DOCKER_REPO
+
+docker manifest inspect \
+	$DOCKER_REPO
